fc0ec8243f3a12fb34dfb803e62dfed123709a3a
lunasaw <iszychen@gmail.com~>
2023-09-30 16:00:10 +0800

    docs

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const ENV = process.env.NODE_ENV;
+
+module.exports = {
+  publicPath: ENV === "development" ? "" : "/xtx-web/",
+  outputDir: 'docs',
+};
+
